a64fd77404d8e8046460888a3fe24f0261bd6bb7
Phi Nguyen <phiwe3296@gmail.com>
2024-05-08 20:06:30 -0400

    added prev try

--- test.py
+++ test.py
@@ -35,6 +35,28 @@ def predict():
 if __name__ == '__main__':
     app.run(debug=True)
 
+# @app.route('/prediction', methods=['POST'])
+# def make_prediction():
+#     data = request.json
+#     array = data['array']
+    
+#     # Convert the JavaScript array to a NumPy array
+#     input_array = np.array(array)
+    
+#     # Reshape the input array if necessary (depends on the model's input shape)
+#     input_array = np.reshape(input_array, (-1, input_array.shape[0]))  # Adjust shape as needed
+    
+#     # Make prediction
+#     prediction = model.predict(input_array)
+    
+#     # Convert prediction to JSON-compatible format
+#     prediction_json = prediction.tolist()
+    
+#     return jsonify({'prediction': prediction_json})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 
 
 
